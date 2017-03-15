e078e4f687a5ef516e61167b96ebf1b2010f754b
Brian James <hellobrianjames@gmail.com>
2017-03-15 13:19:49 -0400

    api abc exercise added

---
+++ cli_sp17/api_intro/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var key = "YOUR_KEY_HERE";
+  var url = "https://api.apixu.com/v1/current.json?key=" + key + "&q=NYC";
+});
+
+
+/*
+    Get weather API key at
+    https://www.apixu.com/signup.aspx
+*/
